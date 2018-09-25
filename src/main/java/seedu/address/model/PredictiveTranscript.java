d63323d8d369a3354e713794a1a2ad4e0482722f
jeremiah-ang <jeremiahang94@gmail.com>
2018-09-25 13:08:19 +0800

    Fix checkstyle

--- src/main/java/seedu/address/model/PredictiveTranscript.java
+++ src/main/java/seedu/address/model/PredictiveTranscript.java
@@ -1,5 +1,9 @@
 package seedu.address.model;
 
+/**
+ * Predictive Transcript is an extension of Transcript but with the
+ * capabilities of coming up with predictions with some extra information given by users
+ */
 public class PredictiveTranscript extends Transcript {
 
     public PredictiveTranscript() {
