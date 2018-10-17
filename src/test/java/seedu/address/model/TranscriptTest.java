05bdcf080463d0ad2e05a0faf155a604f8ae7d37
jeremiah-ang <jeremiahang94@gmail.com>
2018-10-17 15:44:29 +0800

    Update test data

--- src/test/java/seedu/address/model/TranscriptTest.java
+++ src/test/java/seedu/address/model/TranscriptTest.java
@@ -23,41 +23,109 @@ import seedu.address.model.util.ModuleBuilder;
  */
 public class TranscriptTest {
 
-    private static final Module GRADE_BMINUS_4MC_A = new ModuleBuilder()
+    private static final Module COMPLETED_BMINUS_FOURMC_A = new ModuleBuilder()
             .withCode("BMINUSA")
             .withCredit(4)
             .withGrade("B-")
             .build();
-    private static final Module GRADE_A_4MC_A = new ModuleBuilder()
+
+    private static final Module COMPLETED_A_FOURMC_A = new ModuleBuilder()
             .withCode("AA")
             .withCredit(4)
             .withGrade("A")
             .build();
-    private static final Module GRADE_A_4MC_B = new ModuleBuilder()
+    private static final Module COMPLETED_A_FOURMC_B = new ModuleBuilder()
             .withCode("AB")
             .withCredit(4)
             .withGrade("A")
             .build();
-    private static final Module INCOMPLETE_4MC_A = new ModuleBuilder()
+
+    private static final Module INCOMPLETE_FOURMC_A = new ModuleBuilder()
             .withCode("INCOMPLETEA")
             .withCredit(4)
             .withCompleted(false)
             .build();
-    private static final Module INCOMPLETE_4MC_B = new ModuleBuilder()
+    private static final Module INCOMPLETE_FOURMC_B = new ModuleBuilder()
             .withCode("INCOMPLETEB")
             .withCredit(4)
             .withCompleted(false)
             .build();
-    private static final Module INCOMPLETE_4MC_C = new ModuleBuilder()
+    private static final Module INCOMPLETE_FOURMC_C = new ModuleBuilder()
             .withCode("INCOMPLETEC")
             .withCredit(4)
             .withCompleted(false)
             .build();
-    private static final Module INCOMPLETE_5MC_A = new ModuleBuilder()
+
+    private static final Module INCOMPLETE_FIVEMC_A = new ModuleBuilder()
             .withCode("INCOMPLETE5A")
             .withCredit(5)
             .withCompleted(false)
             .build();
+
+    /**
+     * Set A - all incomplete
+     */
+    public final List<Module> MODULES_SET_A_MODULES = new ArrayList<>(Arrays.asList(
+            INCOMPLETE_FOURMC_A,
+            INCOMPLETE_FOURMC_B,
+            INCOMPLETE_FOURMC_C
+    ));
+    public final double MODULES_SET_A_CAP_GOAL = 4.0;
+    public final List<String> MODULES_SET_A_EXPECTED_TARGET_GRADES = new ArrayList<>(Arrays.asList(
+            "B+",
+            "B+",
+            "B+"
+    ));
+
+    /**
+     * Set B - some completed, some incomplete
+     */
+    public final List<Module> MODULES_SET_B_MODULES = new ArrayList<>(Arrays.asList(
+        INCOMPLETE_FOURMC_A,
+        INCOMPLETE_FOURMC_B,
+        INCOMPLETE_FIVEMC_A,
+        INCOMPLETE_FOURMC_C,
+        COMPLETED_BMINUS_FOURMC_A
+    ));
+    public final double MODULES_SET_B_CAP_GOAL = 4.5;
+    public final List<String> MODULES_SET_B_EXPECTED_TARGET_GRADES = new ArrayList<>(Arrays.asList(
+            "A",
+            "A",
+            "A",
+            "A-"
+    ));
+
+    /**
+     * Set C - same modules as Set B but a unachievable goal
+     */
+    public final double MODULES_SET_C_CAP_GOAL = 5.0;
+    public final List<String> MODULES_SET_C_EXPECTED_TARGET_GRADES = null;
+
+    /**
+     * Set D - All completed modules
+     */
+    public final List<Module> MODULES_SET_D_MODULES = new ArrayList<>(Arrays.asList(
+        COMPLETED_BMINUS_FOURMC_A
+    ));
+    public final double MODULES_SET_D_CAP_GOAL = 5.0;
+    public final List<String> MODULES_SET_D_EXPECTED_TARGET_GRADES = new ArrayList<>(Arrays.asList());
+
+    /**
+     * Set E - Very low Goal
+     */
+    public final List<Module> MODULES_SET_E_MODULES = new ArrayList<>(Arrays.asList(
+        INCOMPLETE_FOURMC_A,
+        INCOMPLETE_FOURMC_B,
+        INCOMPLETE_FOURMC_C,
+        COMPLETED_A_FOURMC_A,
+        COMPLETED_A_FOURMC_B
+    ));
+    public final double MODULES_SET_E_CAP_GOAL = 2.0;
+    public final List<String> MODULES_SET_E_EXPECTED_TARGET_GRADES = new ArrayList<>(Arrays.asList(
+            "D",
+            "D",
+            "D"
+    ));
 
     @Test
     public void typicalModulesCapScore() {
@@ -74,57 +142,13 @@ public class TranscriptTest {
 
     @Test
     public void calculateTargetGrades() {
-        List<Module> modules = new ArrayList<>(Arrays.asList(
-            INCOMPLETE_4MC_A,
-            INCOMPLETE_4MC_B,
-            INCOMPLETE_4MC_C
-        ));
-        double capGoal = 4.0;
-        List<String> expectedTargetGrades = new ArrayList<>(Arrays.asList(
-            "B+",
-            "B+",
-            "B+"
-        ));
-        assertTargetGradesEquals(modules, capGoal, expectedTargetGrades);
 
-        modules = new ArrayList<>(Arrays.asList(
-                INCOMPLETE_4MC_A,
-                INCOMPLETE_4MC_B,
-                INCOMPLETE_5MC_A,
-                INCOMPLETE_4MC_C,
-                GRADE_BMINUS_4MC_A
-        ));
-        capGoal = 4.5;
-        expectedTargetGrades = new ArrayList<>(Arrays.asList(
-                "A",
-                "A",
-                "A",
-                "A-"
-        ));
-        assertTargetGradesEquals(modules, capGoal, expectedTargetGrades);
+        assertTargetGradesEquals(MODULES_SET_A_MODULES, MODULES_SET_A_CAP_GOAL, MODULES_SET_A_EXPECTED_TARGET_GRADES);
+        assertTargetGradesEquals(MODULES_SET_B_MODULES, MODULES_SET_B_CAP_GOAL, MODULES_SET_B_EXPECTED_TARGET_GRADES);
+        assertTargetGradesEquals(MODULES_SET_B_MODULES, MODULES_SET_C_CAP_GOAL, MODULES_SET_C_EXPECTED_TARGET_GRADES);
+        assertTargetGradesEquals(MODULES_SET_D_MODULES, MODULES_SET_D_CAP_GOAL, MODULES_SET_D_EXPECTED_TARGET_GRADES);
+        assertTargetGradesEquals(MODULES_SET_E_MODULES, MODULES_SET_E_CAP_GOAL, MODULES_SET_E_EXPECTED_TARGET_GRADES);
 
-        capGoal = 5.0;
-        assertTargetGradesEquals(modules, capGoal, null);
-
-        modules = new ArrayList<>(Arrays.asList(
-                GRADE_BMINUS_4MC_A
-        ));
-        assertTargetGradesEquals(modules, capGoal, new ArrayList<>());
-
-        modules = new ArrayList<>(Arrays.asList(
-                INCOMPLETE_4MC_A,
-                INCOMPLETE_4MC_B,
-                INCOMPLETE_4MC_C,
-                GRADE_A_4MC_A,
-                GRADE_A_4MC_B
-        ));
-        capGoal = 4.0;
-        expectedTargetGrades = new ArrayList<>(Arrays.asList(
-                "B",
-                "B",
-                "B-"
-        ));
-        assertTargetGradesEquals(modules, capGoal, expectedTargetGrades);
     }
 
     /**
